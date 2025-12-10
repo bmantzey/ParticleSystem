8c45ececdd84a4ba351fc987f5ec2a7d342f1c4e
Brandon Mantzey <bmantzey@mac.com>
2025-12-10 17:11:06 -0600

    Basic Project Creation and files generated by Xcode.

---
+++ ParticleEditor/ParticleEditor/ParticleEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleEditorApp.swift
+//  ParticleEditor
+//
+//  Created by Brandon Mantzey on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ParticleEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParticleEditor/ParticleEditorTests/ParticleEditorTests.swift
@@ -0,0 +1,16 @@
+//
+//  ParticleEditorTests.swift
+//  ParticleEditorTests
+//
+//  Created by Brandon Mantzey on 12/10/25.
+//
+
+import Testing
+
+struct ParticleEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
