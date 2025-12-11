0e00f7e1c8ede059a2272b5cc837ec6d543dd624
Brandon Mantzey <bmantzey@mac.com>
2025-12-11 12:57:10 -0600

    Initialize Metal device and command queue in Renderer

--- ParticleEditor/Shared/Rendering/Renderer.swift
+++ ParticleEditor/Shared/Rendering/Renderer.swift
@@ -9,8 +9,25 @@ import MetalKit
 
 final class Renderer: NSObject, MTKViewDelegate {
     
+    private let device: MTLDevice
+    private let commandQueue: MTLCommandQueue
+    
     init(mtkView: MTKView) {
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Metal is not supported on this device.")
+        }
+        self.device = device
+        
+        mtkView.device = device
+        
+        guard let commandQueue = device.makeCommandQueue() else {
+            fatalError("Failed to create Metal command queue.")
+        }
+        self.commandQueue = commandQueue
+        
         super.init()
+        
+        mtkView.delegate = self
     }
     
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
